fc15dca4ad57f353fd10ed54e7f163f77c2e500a
Scott Knell <104751509+sc124578@users.noreply.github.com>
2022-09-27 17:43:33 -0600

    Update polybius.js

--- src/polybius.js
+++ src/polybius.js
@@ -4,48 +4,49 @@
 // of the anonymous function on line 6
 
 const polybiusModule = (function () {
-  // you can add any code you want within this function scope
   const encodeTable = {
-  a: 11, b: 21, c: 31, d: 41, e: 51, 
-  f: 12, g: 22, h: 32, i: 42, j: 42, k: 52,
-  l: 13, m: 23, n: 33, o: 43, p: 53,
-  q: 14, r: 24, s: 34, t: 44, u: 54,
-  v: 15, w: 25, x: 35, y: 45, z: 55
+    a: 11, b: 21, c: 31, d: 41, e: 51,
+    f: 12, g: 22, h: 32, i: 42, j: 42, k: 52, 
+    l: 13, m: 23, n: 33, o: 43, p: 53, 
+    q: 14, r: 24, s: 34, t: 44, u: 54, 
+    v: 15, w: 25, x: 35, y: 45, z: 55,
   }
-
   const decodeTable = {
-  11: "a", 21: "b", 31: "c", 41: "d", 51: "e",
-  12: "f", 22: "g", 32: "h", 42: "(i/j)", 52: "k",
-  13: "l", 23: "m", 33: "n", 43: "o", 53: "p",
-  14: "q", 24: "r", 34: "s", 44: "t", 54: "u",
-  15: "v", 25: "w", 35: "x", 45: "y", 55: "z"  
+    11: "a", 21: "b", 31: "c", 41: "d", 51: "e",
+    12: "f", 22: "g", 32: "h", 42: "(i/j)", 52: "k",
+    13: "l", 23: "m", 33: "n", 43: "o", 53: "p",
+    14: "q", 24: "r", 34: "s", 44: "t", 54: "u",
+    15: "v", 25: "w", 35: "x", 45: "y", 55: "z",
   }
   function polybius(input, encode = true) {
-    let cipherTable = encode ? encodeTable: decodeTable
-   let message = input.toLowerCase()
-   
-   let finalValue = ""
-   console.log(message)
-   for (let i = 0; i < message.length; i++){
-   const letter = message[i]
-   let number = !encode ? message[i] + message[i + 1]: letter
-   console.log("FIND THIS", number)
-   const shifted = cipherTable[number]
-   
-   console.log(shifted)
-   if (!shifted) {
-    
-    finalValue += letter
-   }
-   
-   finalValue += shifted
-   }
-   return finalValue
+    let cipher = input.toLowerCase();
+    let splitString;
+    let finalCipher = encodeTable;
+    if(encode) {
+      splitString = cipher.split("");
+    } else {
+      finalCipher = decodeTable;
+      splitString = cipher.split(" ");
+      const isOdd = splitString.reduce((acc, array) => acc + array.length, 0) % 2;
+      if(isOdd) {
+        return false;
+      }
+      splitString = splitString.map(section => {
+        return section.split("").reduce((acc, char, index, collect) => {
+          if(char === " ") {
+            acc.push(" ");
+          } else if(!(index % 2)) {
+            acc.push(char + collect[index + 1]);
+          }
+          return acc;
+        }, [])
+      })
+      .reduce((a, b) => a.concat(" ", b));
+    }
+    return splitString.map(char => finalCipher[char] || " ").join("");
   }
-
   return {
     polybius,
   };
 })();
-
 module.exports = { polybius: polybiusModule.polybius };
