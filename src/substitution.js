b84da1d78f0da0915880555dd0e405d71828418a
Scott Knell <104751509+sc124578@users.noreply.github.com>
2022-11-01 17:46:51 -0600

    Update substitution.js

--- src/substitution.js
+++ src/substitution.js
@@ -4,10 +4,34 @@
 // of the anonymous function on line 6
 
 const substitutionModule = (function () {
-  // you can add any code you want within this function scope
 
+  const standardAlphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  const cipher = [];
   function substitution(input, alphabet, encode = true) {
-    // your solution code here
+
+    const checkForRepeats = new Set(alphabet);
+    if (alphabet === undefined || alphabet.length < 26 || [...checkForRepeats].length < 26) return false;
+        
+    alphabet.split("");
+    if (encode)
+    {
+      for (let i = 0; i < 26; i++)
+      {
+        cipher[standardAlphabet[i]] = alphabet[i];
+        
+      }
+    } else
+    {
+      for (let i = 0; i < 26; i++)
+      {
+        cipher[alphabet[i]] = standardAlphabet[i];
+      }
+    }
+    let answer = input.toLowerCase().split("").map((letter) => {
+      if (letter === " ") return " ";
+      return cipher[letter]
+    })
+    return answer.join("");
   }
 
   return {
