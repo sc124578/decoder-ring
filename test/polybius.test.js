f5b136efbe920df22f6672c3c9a24acb1b84a158
Scott Knell <104751509+sc124578@users.noreply.github.com>
2022-11-01 17:57:35 -0600

    Update polybius.test.js

--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,6 +1,6 @@
 // Write your tests here!
-const { polybius } = require(".../src/polybius");
-const expect = require("chai")
+const { polybius } = require("../src/polybius");
+const expect = require("chai").expect
 describe("Polybius Shift", () => {
     describe("Encode", () => {
         it("Should encode a messages making each letter a number pair.", () => {
@@ -10,10 +10,10 @@ describe("Polybius Shift", () => {
             expect(actual).to.equal(expected);
         })
         it("Should translate I and J to 42", () => {
-            const messgae = "jinx";
-            const actual = polybius(messgae);
+            const message = "jinx";
+            const actual = polybius(message);
             const expected = "42423335";
-            expect(actual)/to.equal(expected);
+            expect(actual).to.equal(expected);
         })
         it("Should ignore spaces", () => {
             const message = "hi you";
@@ -26,20 +26,20 @@ describe("Polybius Shift", () => {
     describe("Decode", () => {
         it("Should decode a message by making each numbered pair into a letter", () => {
             const message = "23513434112251"
-            const actual = polybius(message);
+            const actual = polybius(message, false);
             const expected = "message";
             expect(actual).to.equal(expected);
         })
         it("Should translate 42 to I and J", () => {
             const message = "42423335";
-            const actual = polybius(message);
+            const actual = polybius(message, false);
             expect(actual).to.include("i");
             expect(actual).to.include("j");
         })
         it("Should ignore spaces", () => {
-            const messgae = "3242 454354";
-            const actual = polybius(message);
-            const expected = "hi you"
+            const message = "2345 23513434112251";
+            const actual = polybius(message, false);
+            const expected = "my message"
             expect(actual).to.equal(expected)
         })
         it("Should return false if length of numbers is odd", () => {
